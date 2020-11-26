75dd5ede94f49c18a2e88ec0e877322d7f58a6a0
birdshiu <qaz0975417436@gmail.com>
2020-11-26 20:25:19 +0800

    Role can move smoothgit add -Agit add -A

--- js/RoleMove.js
+++ js/RoleMove.js
@@ -1,9 +1,12 @@
 var BasicW, BasicH;
 var roleImage=document.getElementsByClassName('role-image')[0];
+var roleX, roleY;
+var isMoving=false;
+var isLeft=true;
 const XStep=20;
 const YStep=10;
 
-var roleAnimation={
+const RoleAnimation={//控制 Role 的動畫
 	isGoOn:false,
 	ImageId:1,
 	aniId:1,
@@ -15,7 +18,7 @@ var roleAnimation={
 			this.aniId=setInterval(function(){
 				self.ImageId=(self.ImageId+1)%5 ? (self.ImageId+1)%5:5;
 				roleImage.src=`./Image/Role/frame-${self.ImageId}.png`;
-			},100);
+			},50);
 		}
 	},
 	stop:function(){
@@ -28,7 +31,56 @@ var roleAnimation={
 };
 		
 function setRole(x, y){
+	roleX=x; roleY=y;
 	let div=document.getElementsByClassName('adventure-div')[0];
 	roleImage.style.top=(div.offsetTop+BasicH*y)+"px";
 	roleImage.style.left=(div.offsetLeft+BasicW*x)+"px";
+}
+
+function walk(x, y){
+	if(roleX === x && roleY === y){
+		RoleAnimation.stop();
+		isMoving=false;
+		return;
+	}
+	if(x > roleX){
+		setTimeout(()=>{
+			setRole(roleX+1, roleY);
+			walk(x, y);
+		}, 150);
+	}
+	else if(x < roleX){
+		setTimeout(()=>{
+			setRole(roleX-1, roleY);
+			walk(x, y);
+		}, 150);
+	}
+	else if(y > roleY){
+		setTimeout(()=>{
+			setRole(roleX, roleY+1);
+			walk(x, y);
+		}, 150);
+	}
+	else if(y < roleY){
+		setTimeout(()=>{
+			setRole(roleX, roleY-1);
+			walk(x, y);
+		}, 150);
+	}
+}
+
+function basicGoTo(x, y){ //先走X再走Y好了
+	if(isMoving === true) return;
+	isMoving=true;
+	RoleAnimation.start();
+	
+	if(x >= roleX){
+		isLeft=true;
+		roleImage.style.transform="";
+	}
+	else{
+		isLeft=false;
+		roleImage.style.transform="scaleX(-1)";
+	}
+	walk(x, y);
 }
