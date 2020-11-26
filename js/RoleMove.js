51714d94e35e4888eb5230872ed4aaa0df618c30
birdshiu <qaz0975417436@gmail.com>
2020-11-26 19:41:25 +0800

    split adventure.html' javascript to RoleMove.js

--- js/RoleMove.js
+++ js/RoleMove.js
@@ -1,15 +1,34 @@
-let Role=(function(){
-	let Img=document.getElementById('role-img');
-	let Div=Img.parentNode;
-	
-	Div.addEventListener('click', (event)=>{
-		let X=event.clientX-Div.offsetLeft+window.pageXOffset;
-		let Y=event.clientY-Div.offsetTop+window.pageYOffset;
+var BasicW, BasicH;
+var roleImage=document.getElementsByClassName('role-image')[0];
+const XStep=20;
+const YStep=10;
 
-		Img.style.left=`${X}`;
-		Img.style.top=`${Y}`;
-	})
-
-	
-})();
-
+var roleAnimation={
+	isGoOn:false,
+	ImageId:1,
+	aniId:1,
+	start:function(){
+		let self=this;
+		console.log('ok');
+		if(self.isGoOn === false){
+			self.isGoOn=true;
+			this.aniId=setInterval(function(){
+				self.ImageId=(self.ImageId+1)%5 ? (self.ImageId+1)%5:5;
+				roleImage.src=`./Image/Role/frame-${self.ImageId}.png`;
+			},100);
+		}
+	},
+	stop:function(){
+		let self=this;
+		if(self.isGoOn === true){
+			self.isGoOn=false;
+			clearInterval(self.aniId);
+		}
+	}
+};
+		
+function setRole(x, y){
+	let div=document.getElementsByClassName('adventure-div')[0];
+	roleImage.style.top=(div.offsetTop+BasicH*y)+"px";
+	roleImage.style.left=(div.offsetLeft+BasicW*x)+"px";
+}
