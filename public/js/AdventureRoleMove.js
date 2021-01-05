03dba483277ba3bc8f82a82cde6719f7b74f7324
birdshiu <qaz0975417436@gmail.com>
2021-01-05 10:20:56 +0800

    more good pathfind

--- public/js/AdventureRoleMove.js
+++ public/js/AdventureRoleMove.js
@@ -4,6 +4,7 @@ var isMoving=false;
 var isLeft=true;
 var rolePath;
 var rolePathIndex;
+var pathDecideHasDone;
 
 const RoleAnimation={//控制 Role 的動畫
 	isGoOn:false,
@@ -40,11 +41,156 @@ function findPath(){
 	rolePath.push({x:roleX, y:roleY});
 	roleMap[roleY][roleX]=1;
 	
+	let routine=new Array(4);
+	
+	function Top(tmpX, tmpY){
+		if(!pathDecideHasDone){
+			if(tmpY-1 >=0 && roleMap[tmpY-1][tmpX] === 0 && roadMap[currentLayer][tmpY-1][tmpX] === 0){
+				rolePath.push({x:tmpX, y:tmpY-1});
+				rolePathIndex++;
+				roleMap[tmpY-1][tmpX]=1;
+				pathDecideHasDone=true;
+			}
+		}
+	}
+	
+	function Right(tmpX, tmpY){
+		if(!pathDecideHasDone){
+			if(tmpX+1 < 20 && roleMap[tmpY][tmpX+1] === 0 && roadMap[currentLayer][tmpY][tmpX+1] === 0){
+				rolePath.push({x:tmpX+1, y:tmpY});
+				rolePathIndex++;
+				roleMap[tmpY][tmpX+1]=1;
+				pathDecideHasDone=true;
+			}
+		}
+	}
+	
+	function Down(tmpX, tmpY){
+		if(!pathDecideHasDone){
+			if(tmpY+1 < 10 && roleMap[tmpY+1][tmpX] === 0 && roadMap[currentLayer][tmpY+1][tmpX] === 0){
+				rolePath.push({x:tmpX, y:tmpY+1});
+				rolePathIndex++;
+				roleMap[tmpY+1][tmpX]=1;
+				pathDecideHasDone=true;
+			}
+		}
+	}
+	
+	function Left(tmpX, tmpY){
+		if(!pathDecideHasDone){
+			if(tmpX-1 >= 0 && roleMap[tmpY][tmpX-1] === 0 && roadMap[currentLayer][tmpY][tmpX-1] === 0){
+				rolePath.push({x:tmpX-1, y:tmpY});
+				rolePathIndex++;
+				roleMap[tmpY][tmpX-1]=1;
+				pathDecideHasDone=true;
+			}
+		}
+		
+	}
+	
+	if(targetX === roleX && targetY < roleY){
+		routine[0]=Top;
+		routine[1]=Right;
+		routine[2]=Left;
+		routine[3]=Down;
+	}
+	else if(targetX > roleX && targetY < roleY){
+		routine[0]=Top;
+		routine[1]=Right;
+		routine[2]=Left;
+		routine[3]=Down;
+	}
+	else if(targetX > roleX && targetY === roleY){
+		routine[0]=Right;
+		routine[1]=Top;
+		routine[2]=Down;
+		routine[3]=Left;
+	}
+	else if(targetX > roleX && targetY > roleY){
+		routine[0]=Right;
+		routine[1]=Down;
+		routine[2]=Top;
+		routine[3]=Left;
+	}
+	else if(targetX === roleX && targetY > roleY){
+		routine[0]=Down;
+		routine[1]=Right;
+		routine[2]=Left;
+		routine[3]=Top;
+	}
+	else if(targetX < roleX && targetY > roleY){
+		routine[0]=Down;
+		routine[1]=Left;
+		routine[2]=Right;
+		routine[3]=Top;
+	}
+	else if(targetX < roleX && targetY === roleY){
+		routine[0]=Left;
+		routine[1]=Top;
+		routine[2]=Down;
+		routine[3]=Right;
+	}
+	else if(targetX < roleX && targetY < roleY){
+		routine[0]=Top;
+		routine[1]=Left;
+		routine[2]=Down;
+		routine[3]=Right;
+	}
+	
 	while( rolePathIndex >= 0){
 		let tmpX=rolePath[rolePathIndex].x;
 		let tmpY=rolePath[rolePathIndex].y;
+		pathDecideHasDone=false;
 		
-		if(tmpY-1 >=0 && roleMap[tmpY-1][tmpX] === 0 && roadMap[currentLayer][tmpY-1][tmpX] === 0){
+			if(targetX === tmpX && targetY < tmpY){
+			routine[0]=Top;
+			routine[1]=Right;
+			routine[2]=Left;
+			routine[3]=Down;
+		}
+		else if(targetX > tmpX && targetY < tmpY){
+			routine[0]=Top;
+			routine[1]=Right;
+			routine[2]=Left;
+			routine[3]=Down;
+		}
+		else if(targetX > tmpX && targetY === tmpY){
+			routine[0]=Right;
+			routine[1]=Top;
+			routine[2]=Down;
+			routine[3]=Left;
+		}
+		else if(targetX > tmpX && targetY > tmpY){
+			routine[0]=Right;
+			routine[1]=Down;
+			routine[2]=Top;
+			routine[3]=Left;
+		}
+		else if(targetX === tmpX && targetY > tmpY){
+			routine[0]=Down;
+			routine[1]=Right;
+			routine[2]=Left;
+			routine[3]=Top;
+		}
+		else if(targetX < tmpX && targetY > tmpY){
+			routine[0]=Down;
+			routine[1]=Left;
+			routine[2]=Right;
+			routine[3]=Top;
+		}
+		else if(targetX < tmpX && targetY === tmpY){
+			routine[0]=Left;
+			routine[1]=Top;
+			routine[2]=Down;
+			routine[3]=Right;
+		}
+		else if(targetX < tmpX && targetY < tmpY){
+			routine[0]=Top;
+			routine[1]=Left;
+			routine[2]=Down;
+			routine[3]=Right;
+		}
+		/*if(tmpY-1 >=0 && roleMap[tmpY-1][tmpX] === 0 && roadMap[currentLayer][tmpY-1][tmpX] === 0){
 			rolePath.push({x:tmpX, y:tmpY-1});
 			rolePathIndex++;
 			roleMap[tmpY-1][tmpX]=1;
@@ -67,8 +213,16 @@ function findPath(){
 		else{
 			rolePath.pop();
 			rolePathIndex--;
+		}*/
+		
+		for(let i=0;i<4;i++){
+			routine[i](tmpX, tmpY);
 		}
-	
+		
+		if(!pathDecideHasDone){
+			rolePath.pop();
+			rolePathIndex--;
+		}
 		if(rolePathIndex < 0){
 			return;
 		}
